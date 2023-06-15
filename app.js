35aac21dd69134342a76d2cab1c44f2f8c4eabfc
Smgy94 <Shanemcgeown@gmail.com>
2023-06-15 15:59:26 +0100

    Added toggle for mobile menu

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const mobileMenuTrigger = document.getElementById("mobileMenuTrigger");
 const navWrapper = document.getElementById("nav-wrapper");
 const navList = document.getElementById("nav-list");
+const barSpans = document.querySelectorAll(".bar");
 
+// Add a click event listener to the mobileMenuTrigger button
 mobileMenuTrigger.addEventListener("click", () => {
-  console.log("Button Clicked");
+  // Toggle the "active" class on mobileMenuTrigger, navWrapper, and navList
   mobileMenuTrigger.classList.toggle("active");
   navWrapper.classList.toggle("active");
   navList.classList.toggle("active");
+
+  // Check if navWrapper has the "active" class to determine the expanded state
+  const isExpanded = navWrapper.classList.contains("active");
+  mobileMenuTrigger.setAttribute("aria-expanded", isExpanded.toString());
+
+  // Toggle the "active" class on each barSpan
+  barSpans.forEach((span) => {
+    span.classList.toggle("active");
+  });
 });
