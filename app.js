060a3ab741369471776115d6a9b2e3d98ed92181
Smgy94 <Shanemcgeown@gmail.com>
2023-06-21 19:40:09 +0100

    commit

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
+// const mobileMenuTrigger = document.getElementById("mobileMenuTrigger");
+// const navWrapper = document.getElementById("nav-wrapper");
+// const navList = document.getElementById("nav-list");
+// const barSpans = document.querySelectorAll(".bar");
+
+// // Add a click event listener to the mobileMenuTrigger button
+// mobileMenuTrigger.addEventListener("click", () => {
+//   // Toggle the "active" class on mobileMenuTrigger, navWrapper, and navList
+//   mobileMenuTrigger.classList.toggle("active");
+//   navWrapper.classList.toggle("active");
+//   navList.classList.toggle("active");
+
+//   // Check if navWrapper has the "active" class to determine the expanded state
+//   const isExpanded = navWrapper.classList.contains("active");
+//   mobileMenuTrigger.setAttribute("aria-expanded", isExpanded.toString());
+
+//   // Toggle the "active" class on each barSpan
+//   barSpans.forEach((span) => {
+//     span.classList.toggle("active");
+//   });
+// });
+
 const mobileMenuTrigger = document.getElementById("mobileMenuTrigger");
 const navWrapper = document.getElementById("nav-wrapper");
 const navList = document.getElementById("nav-list");
 const barSpans = document.querySelectorAll(".bar");
+const body = document.body;
 
 // Add a click event listener to the mobileMenuTrigger button
 mobileMenuTrigger.addEventListener("click", () => {
@@ -18,4 +41,7 @@ mobileMenuTrigger.addEventListener("click", () => {
   barSpans.forEach((span) => {
     span.classList.toggle("active");
   });
+
+  // Toggle the "mobile-menu-open" class on the body
+  body.classList.toggle("mobile-menu-open");
 });
